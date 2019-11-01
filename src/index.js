17d1381db516c92187c021bcbbf9e59d6ab7746f
John Schoeman <johnschoeman1617@gmail.com>
2019-11-01 15:59:14 -0400

    Add tic tac toe with rxjs
    This commit adds a tic tac toe game built using rxjs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import initSolarSystem from "./solar_system"
 import initTree from "./tree"
 import initAsteroids from "./Asteroids"
 import initSolarSystem2 from "./SolarSystem2"
+import initTicTacToe from "./TicTacToe"
 
 window.addEventListener("DOMContentLoaded", () => {
   switch (document.title) {
@@ -44,6 +45,10 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     case "SolarSystem2": {
       initSolarSystem2()
+      break
+    }
+    case "TicTacToe": {
+      initTicTacToe()
       break
     }
   }
