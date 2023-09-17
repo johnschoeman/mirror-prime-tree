6f87292c77d39250804d6fb1baad0b2d13a2b159
John Schoeman <johnschoeman1617@gmail.com>
2023-09-17 12:07:53 -0400

    Add build logs

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,10 @@ cp -r public/* build
 cp -r src/markup build/doodles
 
 # Build
+echo "Building css..."
 npx tailwindcss -i ./src/tailwind.css -o ./build/index.css
+
+echo "Building js..."
 node bin/esbuild.js
 
 
