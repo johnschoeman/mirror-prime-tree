e97762f8ad2506eed3fd4de43ce2edd6862037db
John Schoeman <johnschoeman1617@gmail.com>
2023-09-17 12:11:11 -0400

    Move css to git repo

--- bin/build.sh
+++ bin/build.sh
@@ -8,10 +8,6 @@ cp -r public/* build
 cp -r src/markup build/doodles
 
 # Build
-echo "Building css..."
-npx tailwindcss -i ./src/tailwind.css -o ./build/index.css
-
-echo "Building js..."
 node bin/esbuild.js
 
 
